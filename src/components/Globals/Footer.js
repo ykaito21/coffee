a9a6c2585bb5d1be60c53155731fe0032c693415
Yuki Kaito <ykaito21@gmail.com>
2019-02-23 15:56:04 +0900

    add footer

---
+++ src/components/Globals/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer py-3">
+      <div className="container">
+        <div className="rwo">
+          <div className="col-10 mx-auto col-md-6 text-yellow text-center text-capitalize">
+            <h3>
+              all right reserved &copy; {new Date().getFullYear().toString()}
+            </h3>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/components/layout.js
+++ src/components/layout.js
@@ -6,11 +6,13 @@ import "./layout.css";
 import "./bootstrap.min.css";
 
 import Navbar from "./Globals/Navbar";
+import Footer from "./Globals/Footer";
 
 const Layout = ({ children }) => (
   <>
     <Navbar />
     {children}
+    <Footer />
   </>
 );
 
